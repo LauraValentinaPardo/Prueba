ff8cef2f7ba8c7f9867101ea088c10613853e7e4
Valen <valenvrg@gmail.com>
2022-02-17 14:16:25 -0500

    Temperatura

---
+++ Ramas.py
@@ -0,0 +1,20 @@
+Agua= 20
+Temperatura=15
+if Agua >= 20:
+    print("El nivel de agua esta bien")
+elif Agua <= 10:
+    print("Precaucion el nivel del agua es bajo")
+elif Agua <= 0:
+    print("Peligro ya no hay agua")
+    
+    
+    
+if Temperatura == 15:
+    print("Temperatura optima")
+elif Temperatura >= 16:
+    print("Temperatura es muy alta")
+elif Temperatura <= 14:
+    print("Temperatura muy baja")
+    
+    
+    
