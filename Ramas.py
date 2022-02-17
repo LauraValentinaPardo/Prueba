36ccc66784ee4a5d1c17f548a5488d6094760996
Valen <valenvrg@gmail.com>
2022-02-17 14:21:00 -0500

    Gato

--- Ramas.py
+++ Ramas.py
@@ -1,5 +1,6 @@
 Agua= 20
 Temperatura=15
+Sensor = 0
 if Agua >= 20:
     print("El nivel de agua esta bien")
 elif Agua <= 10:
@@ -15,6 +16,11 @@ elif Temperatura >= 16:
     print("Temperatura es muy alta")
 elif Temperatura <= 14:
     print("Temperatura muy baja")
+    
+if Sensor ==0:
+    print("Todo va bien, ningun gato cerca")
+elif Sensor >=1:
+    print("Peligro hay un gato cerca")
     
     
     
